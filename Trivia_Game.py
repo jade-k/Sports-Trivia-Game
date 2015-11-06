09a817028a065a87738186feb8a894fc3e4d121c
jade-k <programmer@hs-lab-5.local>
2015-11-06 08:09:04 -0500

    ...

--- Trivia_Game.py
+++ Trivia_Game.py
@@ -89,8 +89,10 @@ def display_question (question, answer, correct): #Patrick
 
 def random_shuffle (q_and_a): #Jade
     """Takes the questions,  puts them into  a random order"""
-    For q_and_a=
-    [('Who has the most total touchdowns on the New England Patriots as of 2015?','rob gronkowski'),('Which country won the 2015 Womens FIFA World Cup?','united states'),('What is the name of the penalty in hockey when a player hits another player with the hockey stick?','high stick'),('What is Bostons NBA basketball team?','celtics'),('In basketball what is a basket from the court halfline worth?','3 points'),('Which MLB baseball team won the World Series in 2013?','red sox'),('What is three strikes in a row in bowling called?','hamstring'),('In track and field what is the event where someone jumps over obstacles while racing?','hurdles'),('What is the name of Americas largest skateboarding conversation?','x games'),('What does NASCAR stand for?','national association for stock car auto racig')]
+    For q_and_a =
+    [('Who has the most total touchdowns on the New England Patriots as of 2015?','rob gronkowski'),('Which country won the 2015 Womens FIFA World Cup?','united states'),('What is the name of the penalty in hockey when a player hits another player with the hockey stick?','high stick'),('What is Bostons NBA basketball team?','celtics'),('In basketball what is a basket from the court halfline worth?','3 points'),('Which MLB baseball team won the World Series in 2013?','red sox'),('What is three strikes in a row in bowling called?','turkey'),('In track and field what is the event where someone jumps over obstacles while racing?','hurdles'),('What is the name of Americas largest skateboarding conversation?','x games'),('What does NASCAR stand for?','national association for stock car auto racig')]
+
+q_and_a = questions_and_answers
 
 def display_results (answers, correct, incorrect): #Patrick
     """Display correct answers out of total answers"""
@@ -112,8 +114,8 @@ def main_loop (): #Jade
         question = q_and_a[0]
         answer = q_and_a[1]
         display_question(question, answer)
-        # Add to results ???
     display_results(answers)
+    results = []
     play_game(display_title, randomize_questions, display_questions, display_results)
 
 main_loop()
